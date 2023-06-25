0f39ee8d108cf8c1f06f72aaa7c0d961e6d829d8
Darlan <darlandc@gmail.com>
2023-06-24 22:38:11 -0300

    feat: iniciando projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-html-generator';
+
+  constructor(private renderer: Renderer2) { }
+
+  generatePage() {
+    const homePageContent = '<h1>Página inicial</h1>'; // Conteúdo da página inicial
+    const aboutPageContent = '<h1>Sobre</h1>'; // Conteúdo da página "Sobre"
+
+    this.create('home.html', homePageContent);
+    this.create('about.html', aboutPageContent);
+  }
+
+  create(nomeArquivo: string, conteudo: string) {
+    const element = this.renderer.createElement('a');
+    element.setAttribute('href', `data:text/html;charset=UTF-8,${encodeURIComponent(conteudo)}`);
+    element.setAttribute('download', nomeArquivo);
+    this.renderer.setStyle(element, 'display', 'none');
+    this.renderer.appendChild(document.body, element);
+    element.click();
+    this.renderer.removeChild(document.body, element);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { Renderer2 } from '@angular/core';
 
 @NgModule({
   declarations: [
@@ -11,7 +10,7 @@ import { Renderer2 } from '@angular/core';
   imports: [
     BrowserModule
   ],
-  providers: [Renderer2],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
